85051a596467dbb58c35de94dde6fc5214332b32
Antype Cryptous <3c7975@gmail.com>
2022-08-08 00:33:50 +0800

    updated scripts

--- app.py
+++ app.py
@@ -11,22 +11,26 @@ class ChatBot:
         self.wake_up = False
         self.nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
 
-    def welcome_prompt(self):
+    def welcome_prompt(self, init_msg):
         self.wake_up = True
-        res = np.random.choice([f"Hello, I'm {self.name}, how can I help you?", f"Hello there, I'm {self.name} the AI, how may I assist you?", f"Sup, my name is {self.name}, how can I help you?"])
-        return res
+        if self.name.lower() in init_msg.lower():
+            res = np.random.choice([f"Hello I am {self.name} the AI, what can I do for you?", f"Hello, my name is {self.name} the AI. What can I help you with?"])
+            return res
+        else:
+            res = self.generate_response(init_msg)
+            return res
 
     def generate_response(self, feed):
         if any(i in feed.lower() for i in ["what's your name", "what is your name"]):
-            res = np.random.choice([f"I'm {ai.name}.", f"My name is {ai.name}."])        
+            res = np.random.choice([f"I'm {self.name}.", f"My name is {self.name}."])        
         else:
             chatter = self.nlp(transformers.Conversation(feed), pad_token_id=50256)
             res = str(chatter)
             res = res[res.find("bot >> ")+6:].strip()
         return res
     
-    def text_to_speech(self, input_res):
-        tts_en = gTTS(text=input_res, lang="en", slow=False)
+    def text_to_speech(self, text):
+        tts_en = gTTS(text=text, lang="en", slow=False)
         filename = "res"
         tts_en.save(filename+".mp3")
         os.system(f"start {filename}.mp3")
@@ -37,18 +41,14 @@ class ChatBot:
 if __name__ == '__main__':
     ai = ChatBot(name="Dev")
     ai.clear_console()
-    input("user >> ")
 
     while True:
         if ai.wake_up == True:
-            r = ai.generate_response(user_res)
-            
-            print(r)
-            ai.text_to_speech(r)
             user_res = input("user >> ")
+            r = ai.generate_response(user_res)
+            print(r)
 
         else:
-            r = ai.welcome_prompt()
-            print(r)
-            ai.text_to_speech(r)
             user_res = input("user >> ")
+            r = ai.welcome_prompt(user_res)
+            print(r)
--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from xml.dom.minidom import parseString
 import numpy as np
 from app import ChatBot
 
@@ -6,10 +7,11 @@ class GUI:
     def __init__(self, master, ai):
         self.master = master
         master.title(ai.name)
-        master.geometry("400x600")
-        self.ai = ai
-        self.user_input = ""
+        master.geometry('450x600')
 
+        self.ai = ai
+        self.user_input = ''
+        # self.line_count = 0
         self.layout()
     
     def layout(self):
@@ -18,24 +20,46 @@ class GUI:
         l1.pack()
 
         self.text_box = Text(self.master, height=30, width=40)
+        self.text_box.config(state=DISABLED)
         self.text_box.pack(fill="both", expand=True)
 
         self.entry_line = Entry(self.master)
         self.entry_line.pack(fill="x")
+        self.enter_func()
 
-        self.entry_line.insert(0, np.random.choice(["Chat with me...", "Dispatch me...", "Converse with me..."]))
-        self.entry_line.bind("<FocusIn>", self.entry_line.delete(0, "end"))
+    def enter_func(self):
+        temp_text = ["Chat with me...", "Dispatch me...", "Converse with me..."]
+        self.entry_line.insert(0, np.random.choice(temp_text))
+        self.entry_line.bind("<FocusIn>", self.del_temp_text)
         self.entry_line.bind("<Return>", self.output_res)
 
-    def output_res(self):
+    def del_temp_text(self, *args, **kwargs):
+        self.entry_line.delete(0, 'end')
+        # print(*args, **kwargs)
+
+    def output_res(self, *args, **kwargs):
         self.user_input = self.entry_line.get()
+        self.entry_line.delete(0, 'end')
+        self.enter_func()
+        # self.line_count += 1
 
         if self.ai.wake_up == True:
             r = self.ai.generate_response(self.user_input)
-            self.text_box.insert(END, r + '\n')
+            self.text_box['state'] = NORMAL
+            # self.text_box.insert(END, f'{self.line_count}. {r}\n')
+            self.text_box.insert(END, f"user >> {self.user_input}\n")
+            self.text_box.insert(END, f"{self.ai.name} >> {r}\n")
+            self.text_box['state'] = DISABLED
+            self.ai.text_to_speech(r)
         else:
-            r = self.ai.welcome_prompt()
-            self.text_box.insert(END, r + '\n')
+            r = self.ai.welcome_prompt(self.user_input)
+            self.text_box['state'] = NORMAL
+            # self.text_box.insert(END, f'{self.line_count}. {r}\n')
+            self.text_box.insert(END, f"user >> {self.user_input}\n")
+            self.text_box.insert(END, f"{self.ai.name} >> {r}\n")
+            self.text_box['state'] = DISABLED
+            self.ai.text_to_speech(r)
+        # print(*args, **kwargs)
 
 root = Tk()
 GUI(root, ChatBot(name="Dev"))
